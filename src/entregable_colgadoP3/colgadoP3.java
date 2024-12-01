d806fa0c75595defd3ad131ce8a52adf10d7c588
Santi-Corrales <santi.developer@hotmail.com>
2024-12-01 23:37:37 +0100

    Creación segundo bucle principal + condiciones
    Creamos un bucle WHILE el cual se ejecutará mientras la palabra no sea adivinada. Dentro del bucle tendremos condicionales IF que se ejecutarán si el jugador acierta, no acierta o si se ha quedado sin intentos. Finalmente printamos que se ha terminado.

--- src/entregable_colgadoP3/colgadoP3.java
+++ src/entregable_colgadoP3/colgadoP3.java
@@ -73,7 +73,7 @@ public class colgadoP3 {
 			System.out.println(" ");
 			System.out.println("De acuerdo, el número de rondas serán " + rondas);
 			//bucle principal que proporciona las rondas elegidas 
-		for (int ronda = 1; ronda <= rondas; ronda++) {
+			for (int ronda = 1; ronda <= rondas; ronda++) {
 				System.out.println("Ronda " + ronda);
 
 				Scanner palabra = new Scanner(System.in);
@@ -82,17 +82,53 @@ public class colgadoP3 {
 
 				Scanner letras = new Scanner(System.in);
 
-				//definimos nuestras variables
+				// definimos nuestras variables
 				String palabraSecreta = palabraS;
 				int intentosMaximos = 6;
 				int intentos = 0;
 				int palabraAdivinada = 0;
-				
-				//iniciamos nuestro primer Array para las letras adivinadas, seguidamente el FOR representará con guión bajo las letras no adivinadas
+
+				// iniciamos nuestro primer Array para las letras adivinadas, seguidamente el
+				// FOR representará con guión bajo las letras no adivinadas
 				char[] letrasAdivinadas = new char[palabraSecreta.length()];
 				for (int i = 0; i < letrasAdivinadas.length; i++) {
 					letrasAdivinadas[i] = '_';
 				}
+				// comienza un bucle while que continuará mientras no se adivine la palabra o no se alcancen los intentos máximos
+				while (palabraAdivinada == 0 && intentos < intentosMaximos) {
+					System.out.println("Palabra a adivinar: " + String.valueOf(letrasAdivinadas) + " ("
+							+ palabraSecreta.length() + " letras)");
+					System.out.println("Introduce una letra, por favor");
+					// lee la letra pasandolas a minúsculas y recogiendo solo la posición 0
+					char letra = Character.toLowerCase(letras.next().charAt(0));
+
+					int letraCorrecta = 0;
+					// con este for recorremos la palabra secreta y comprueba si la letra ingresada
+					// equivale a una de las letras de la palabra
+					for (int i = 0; i < palabraSecreta.length(); i++) {
+						if (palabraSecreta.charAt(i) == letra) {
+							letrasAdivinadas[i] = letra;
+							letraCorrecta = 1;
+						}
+					}
+					// si no se encuentra la letra, se incrementa el contador de intentos.
+					if (letraCorrecta == 0) {
+						intentos++;
+						System.out.println("¡Incorrecto! Te quedan " + (intentosMaximos - intentos) + " intentos.");
+					}
+					// si todas las letras han sido adivinadas, ganará el jugador 2
+					if (String.valueOf(letrasAdivinadas).equals(palabraSecreta)) {
+						palabraAdivinada = 1;
+						System.out.println(
+								"¡Felicidades " + nombre2 + "! Has adivinado la palabra secreta: " + palabraSecreta);
+					}
+				}
+				// condicional para cuando no haya mas intentos
+				if (palabraAdivinada == 0) {
+					System.out.println("¡" + nombre2 + " te has quedado sin intentos!");
+				}
+			}
+			System.out.println("¡Gracias por jugar! El juego ha terminado.");
 			break;
 		}
 	
